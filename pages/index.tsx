726c2fb3cdf65f650cb858af6889307524bfbf5d
Nabeel Sheikh <nabeel@nabeels-mbp-2.lan>
2022-01-13 20:46:49 -0600

    changed icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,7 +38,8 @@ const Home: NextPage = ({ data }) => {
     <div className="font-mono text-offwhite flex flex-col items-center justify-center min-h-screen">
       <Head>
         <title>Critterz Tracker</title>
-        <meta property="og:image" content={`/critterz.gif`} />
+        <meta property="og:image" content={`/ico.svg`} />
+        <meta property="twitter:image" content={`/ico.svg`} />
         <meta property="og:url" content="https://www.critterztracker.com" />
         <meta property="og:determiner" content="Critterz Tracker" />
         <meta name="twitter:creator" content="@_Nabs__" />
